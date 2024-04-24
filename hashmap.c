e261b0704a5567347ef3289afdc4342752776eca
JCGeraldo <163364975+JCGeraldo@users.noreply.github.com>
2024-04-24 02:17:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -130,7 +130,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
+    if(map==NULL) return NULL;
+    for(unsigned long i=0;i<map->capacity;i++){
+        if(map->buckets[i]!=NULL){
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
     return NULL;
 }
 
